81e11dd942a9ab3ecd09956649419a3bb0a7ef90
ridoan777 <ridoan777kabir@gmail.com>
2025-06-27 23:31:03 +0600

    starter: vue+pinia+persist+tailwind : 1.0

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { RouterLink, RouterView } from 'vue-router'
+
+import ChevronUp from 'vue-material-design-icons/ChevronUp.vue';
+import ChevronDown from 'vue-material-design-icons/ChevronDown.vue';
+import ChevronRight from 'vue-material-design-icons/ChevronRight.vue';
+import ChevronLeft from 'vue-material-design-icons/ChevronLeft.vue';
+
+let openMenu = ref(false)
+</script>
+
+<template>
+  
+</template>
